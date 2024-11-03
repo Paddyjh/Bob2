c33720fe7fd3504e04d1d7ef568a6225b6898c32
itzVort3x1 <saikaustubh10@gmail.com>
2024-11-03 10:31:17 +0000

    added AES encryption and also updated the testcases to verify encryption.

--- src/protocol/bob2_protocol.py
+++ src/protocol/bob2_protocol.py
@@ -1,11 +1,45 @@
 import struct
 import socket
 import zlib
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
 
 class Bob2Protocol:
-    def __init__(self, version_major=0, version_minor=0):
+    def __init__(self, version_major=0, version_minor=0, encryption_key=None):
         self.version_major = version_major
         self.version_minor = version_minor
+
+        if encryption_key is None or len(encryption_key) != 32:
+            raise ValueError("Encryption key must be a 32-byte (256-bit) value")
+        self.encryption_key = encryption_key
+
+    def encrypt_message(self, plaintext):
+        if isinstance(plaintext, str):
+            plaintext = plaintext.encode()
+
+        iv = os.urandom(16)
+        cipher = Cipher(algorithms.AES(self.encryption_key), modes.CBC(iv), backend=default_backend())
+        encryptor = cipher.encryptor()
+
+        padder = padding.PKCS7(algorithms.AES.block_size).padder()
+        padded_plaintext = padder.update(plaintext) + padder.finalize()
+
+        encrypted_message = iv + encryptor.update(padded_plaintext) + encryptor.finalize()
+        return encrypted_message
+
+    def decrypt_message(self, encrypted_message):
+        iv = encrypted_message[:16]
+        ciphertext = encrypted_message[16:]
+        cipher = Cipher(algorithms.AES(self.encryption_key), modes.CBC(iv), backend=default_backend())
+        decryptor = cipher.decryptor()
+
+        padded_plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+
+        unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
+        plaintext = unpadder.update(padded_plaintext) + unpadder.finalize()
+        return plaintext
 
     def build_message(self, message_type, dest_ipv6, dest_port, message_content):
         try:
@@ -16,19 +50,24 @@ class Bob2Protocol:
         header = struct.pack('!BBB', self.version_major, self.version_minor, message_type)
         dest_port_bytes = struct.pack('!H', dest_port)
 
-        message_length = len(message_content)
+        encrypted_content = self.encrypt_message(message_content)
+
+        message_length = len(encrypted_content)
         if message_length > (1 << 40) - 1:
             raise ValueError("Message content exceeds maximum allowed size")
 
         length_bytes = message_length.to_bytes(5, byteorder='big')
-        checksum = zlib.crc32(message_content.encode('utf-8'))
+        checksum = zlib.crc32(encrypted_content)
         checksum_bytes = struct.pack('!I', checksum)
 
         full_message = (header + dest_ip_bytes + dest_port_bytes + length_bytes +
-                        checksum_bytes + message_content.encode('utf-8'))
+                        checksum_bytes + encrypted_content)
         return full_message
 
     def parse_message(self, raw_data):
+        if len(raw_data) < 30:
+            raise ValueError("Insufficient data for parsing")
+
         version_major, version_minor, message_type = struct.unpack('!BBB', raw_data[:3])
         dest_ip_bytes = raw_data[3:19]
         dest_ipv6 = socket.inet_ntop(socket.AF_INET6, dest_ip_bytes)
@@ -36,8 +75,14 @@ class Bob2Protocol:
         message_length = int.from_bytes(raw_data[21:26], byteorder='big')
 
         expected_checksum = struct.unpack('!I', raw_data[26:30])[0]
-        message_content = raw_data[30:30 + message_length]
-        actual_checksum = zlib.crc32(message_content)
+        encrypted_content = raw_data[30:]
+
+        if len(encrypted_content) != message_length:
+            raise ValueError("Length of encrypted content does not match the message length")
+
+        decrypted_content = self.decrypt_message(encrypted_content)
+
+        actual_checksum = zlib.crc32(encrypted_content)
 
         if expected_checksum != actual_checksum:
             raise ValueError("Checksum verification failed")
@@ -50,5 +95,5 @@ class Bob2Protocol:
             "destination_port": dest_port,
             "message_length": message_length,
             "checksum": expected_checksum,
-            "message_content": message_content.decode('utf-8')
+            "message_content": decrypted_content.decode('utf-8')
         }
--- test/test_bob2_protocol.py
+++ test/test_bob2_protocol.py
@@ -1,9 +1,14 @@
 import unittest
+# Use the following two lines only on Windows, as the project path was not recognized
+import sys
+sys.path.append('../')
 from src.protocol.bob2_protocol import Bob2Protocol
+import os
 
 class TestBob2Protocol(unittest.TestCase):
     def setUp(self):
-        self.bob2 = Bob2Protocol()
+        self.encryption_key = os.urandom(32)
+        self.bob2 = Bob2Protocol(encryption_key=self.encryption_key)
 
     def test_message_build_and_parse(self):
         message_content = "Test Message"
@@ -15,6 +20,22 @@ class TestBob2Protocol(unittest.TestCase):
         )
         parsed_message = self.bob2.parse_message(message)
         self.assertEqual(parsed_message["message_content"], message_content)
+
+    def test_encryption(self):
+        message_content = "This is a test message"
+        encrypted_message = self.bob2.build_message(
+            message_type=1,
+            dest_ipv6="2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+            dest_port=12345,
+            message_content=message_content
+        )
+
+        self.assertNotIn(message_content.encode('utf-8'), encrypted_message, 
+                         "Message content should be encrypted and not appear in plaintext")
+
+        parsed_message = self.bob2.parse_message(encrypted_message)
+        self.assertEqual(parsed_message["message_content"], message_content,
+                         "Decrypted message content should match the original")
 
 if __name__ == "__main__":
     unittest.main()
