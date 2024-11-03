da7cecde152aee9ec9580580dd859324c5b51aef
itzVort3x1 <saikaustubh10@gmail.com>
2024-11-03 21:16:10 +0000

    updated main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from protocol.bob2_protocol import Bob2Protocol
+import os
 
 if __name__ == "__main__":
-    bob2 = Bob2Protocol()
+    bob2 = Bob2Protocol(encryption_key=os.urandom(32))
     message = bob2.build_message(
         message_type=0,
         dest_ipv6="2001:0db8:85a3:0000:0000:8a2e:0370:7334",
