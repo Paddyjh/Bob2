511d2d66de112715ef7d5d82ce8ac7087b2eb31c
Claire Gregg <clairegregg@clairegregg.com>
2024-11-05 12:28:40 +0000

    Add packet number to protocol

--- src/protocol/bob2_protocol.py
+++ src/protocol/bob2_protocol.py
@@ -7,13 +7,18 @@ class Bob2Protocol:
         self.version_major = version_major
         self.version_minor = version_minor
 
-    def build_message(self, message_type, dest_ipv6, dest_port, message_content):
+    def build_message(self, message_type, dest_ipv6, dest_port, message_content, multiple_packets=False, packet_num=0):
         try:
             dest_ip_bytes = socket.inet_pton(socket.AF_INET6, dest_ipv6)
         except socket.error:
             raise ValueError("Invalid IPv6 address")
 
         header = struct.pack('!BBB', self.version_major, self.version_minor, message_type)
+        if multiple_packets:
+            packet_bytes = packet_num.to_bytes(2, byteorder='big')
+        else:
+            packet_bytes = bytes(2)
+
         dest_port_bytes = struct.pack('!H', dest_port)
 
         message_length = len(message_content)
@@ -24,25 +29,28 @@ class Bob2Protocol:
         checksum = zlib.crc32(message_content.encode('utf-8'))
         checksum_bytes = struct.pack('!I', checksum)
 
-        full_message = (header + dest_ip_bytes + dest_port_bytes + length_bytes +
+        full_message = (header + packet_bytes + dest_ip_bytes + dest_port_bytes + length_bytes +
                         checksum_bytes + message_content.encode('utf-8'))
         return full_message
 
     def parse_message(self, raw_data):
         version_major, version_minor, message_type = struct.unpack('!BBB', raw_data[:3])
-        dest_ip_bytes = raw_data[3:19]
+        packet_num = int.from_bytes(raw_data[3:5], byteorder='big')
+        if packet_num == 0:
+            multiple_packets = False
+        dest_ip_bytes = raw_data[5:21]
         dest_ipv6 = socket.inet_ntop(socket.AF_INET6, dest_ip_bytes)
-        dest_port = struct.unpack('!H', raw_data[19:21])[0]
-        message_length = int.from_bytes(raw_data[21:26], byteorder='big')
+        dest_port = struct.unpack('!H', raw_data[21:23])[0]
+        message_length = int.from_bytes(raw_data[23:28], byteorder='big')
 
-        expected_checksum = struct.unpack('!I', raw_data[26:30])[0]
-        message_content = raw_data[30:30 + message_length]
+        expected_checksum = struct.unpack('!I', raw_data[28:32])[0]
+        message_content = raw_data[32:32 + message_length]
         actual_checksum = zlib.crc32(message_content)
 
         if expected_checksum != actual_checksum:
             raise ValueError("Checksum verification failed")
 
-        return {
+        response =  {
             "version_major": version_major,
             "version_minor": version_minor,
             "message_type": message_type,
@@ -52,3 +60,9 @@ class Bob2Protocol:
             "checksum": expected_checksum,
             "message_content": message_content.decode('utf-8')
         }
+        if multiple_packets:
+            response["packet_num"] = packet_num
+        else:
+            response['multiple_packets'] = False
+
+        return response
